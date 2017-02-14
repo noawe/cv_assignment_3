70e72e1d8e9aae6964b07a83d5e9ba3b258a04e0
Roland Gritzer <gritzer.roland@gmail.com>
2017-02-14 16:34:33 +0100

    random & output

--- main.m
+++ main.m
@@ -7,15 +7,21 @@ global fig;
 global images;
 global current;
 
-folder = 'img/mini_airplanes/';
 current = 1;
 images = [];
-trainset_size = 100;
+trainset_size = 75;
 bow_size = 100; % size of bow dictionary
 
-%build datasets with labels and filenames
-plane_set = struct("label", 1, "file", glob(strcat('img/planes/', '*.jpg')));
-car_set = struct("label", -1, "file", glob(strcat('img/cars/', '*.jpg')));
+% type
+% 1 = plane
+% -1 = car
+
+%build datasets with types and filenames
+plane_set = struct("type", 1, "file", glob(strcat('img/planes/', '*.jpg')));
+car_set = struct("type", -1, "file", glob(strcat('img/cars/', '*.jpg')));
+
+plane_set = plane_set(randperm(length(plane_set)));
+car_set = car_set(randperm(length(car_set)));
 
 %split into training and test images
 train_set = vertcat(plane_set(1:trainset_size), car_set(1:trainset_size));
@@ -40,7 +46,7 @@ endfunction
 
 function features = get_features(img)
 	% gabor 
-	% TODO: find working parameters...
+	% TODO: f working parameters...
 	%  img = GaborEnergy(img, 4, 9, 9, 2, 0, 0);  
 
 	% sift
@@ -52,7 +58,7 @@ function [bow, model] = train (train_set, bow_size)
 
 	features = [];
 	num_features = [];
-	train_labels = [];
+	train_types = [];
 
 	for j = 1 : length(train_set)
 	  img = prepare_image (train_set(j).file);
@@ -61,7 +67,7 @@ function [bow, model] = train (train_set, bow_size)
 	  img_features = get_features(img);
 	  features = horzcat(features, img_features); % add feature descriptors
 	  num_features = [num_features ; size(img_features, 2)]; % save number of features per image
-	  train_labels =  [train_labels; train_set(j).label];
+	  train_types =  [train_types; train_set(j).type];
 	endfor
 
 	%vector quantization
@@ -78,7 +84,7 @@ function [bow, model] = train (train_set, bow_size)
 		train_instances  = [train_instances ; histc(assignments(: , (index(i)+1):index(i+1) ), 1:bow_size)];
 	endfor
 
-	model = svmtrain(double(train_labels), double(train_instances), '-s 0 -t 0 -c 1');
+	model = svmtrain(double(train_types), double(train_instances), '-s 0 -t 0 -c 1');
 
 endfunction
 
@@ -96,20 +102,46 @@ function test_instance = get_test_instance (filename, bow)
 
 endfunction
 
+function type = get_type(n)
+  if (n == 1)
+    type = 'plane';
+  elseif
+    type = 'car';
+  endif
+endfunction
+
 function test (test_set, bow, model)
+  
+  % Positive = Plane
+  TP = 0; TN = 0; FP = 0; FN = 0;
 
 	for i = 1:length(test_set)
 		t = get_test_instance (test_set(i).file, bow);
-		svmpredict(test_set(i).label, double(rot90(t)), model)
-	endfor
+		[predicted_type] = svmpredict(test_set(i).type, double(rot90(t)), model, '-q');
+    
+    % print results & count
+    result_string = 'Test Result: ';
+    if (test_set(i).type == predicted_type)
+      result_string = [result_string, 'correct ('];
+      if (predicted_type == 1) TP++; elseif TN++; endif;
+    elseif
+      result_string = [result_string, 'FAILED ('];
+      if (predicted_type == 1) FP++; elseif FN++; endif;
+    endif
+    result_string = [result_string, get_type(predicted_type), ')'];
+    disp(result_string);
+  endfor
+  
+  disp('#############################');
+  disp(['Tested ', num2str(i) , ' pictures']);
+  disp(['TP: ', num2str(TP), ' TN: ', num2str(TN), ' FP: ', num2str(FP), ' FN: ', num2str(FN)]);
+  success_rate = (TP + TN) / i;
+  disp(['success rate: ', num2str(success_rate)]);
 
 endfunction
-
 
 %training
 [bow, model] = train (train_set, bow_size);
 
 %testing
 test (test_set, bow, model);
-
-
